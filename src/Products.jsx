4564b2d1c1c779e0dc280a916afe82dcb89c5c71
Santsosh Khadka <santoshkhadka2326@gmail.com>
2023-12-25 13:28:19 +0545

    small change

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 const Products = () => {
   return (
-    <div>
-      
+    <div className="flex overflow-hidden bg-slate-100">
+        <NavBar/>
+        <div className="flex-col w-4/5">
+          <div className="mt-5 mr-20">
+              <p className="text-2xl">Welcome Back, Deepak</p>
+          </div>
+          <div>
+            <label htmlFor="101"></label>
+            <input type="search" name="search" id="101" />
+          </div>
+    </div>
     </div>
   )
 }
 
-export default Products
+export default Products;
