39cd7e6c622f01e3cf45b0048d859b57f1444754
Santsosh Khadka <santoshkhadka2326@gmail.com>
2023-12-25 12:21:00 +0545

    mistake-improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import NavBar from "./components/Home";
-
+import Home from "./components/Home";
 function App() {
   return (
     <>
-      <NavBar />
+      <Home />
     </>
   );
 }
